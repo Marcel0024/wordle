2a6c52061a0a8ced46addc207e7cec0945aa675d
Marcel Croes <14852157+Marcel0024@users.noreply.github.com>
2022-02-02 20:13:51 +0100

    Backspace instead of backspace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -94,6 +94,6 @@ export class AppComponent implements OnInit {
     ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
     ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
     ['Z', 'X', 'C', 'V', 'B', 'N', 'Ñ', 'M'],
-    ['ENTER', 'BACK'],
+    ['ENTER', 'BACKSPACE'],
   ];
 }
